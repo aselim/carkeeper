d9b7360b4f228e5909a06096e739947c10f9ed38
Ahmed A. Selim <ahmed.s3lim@gmail.com>
2012-12-29 08:32:38 -0800

    modifing the users tables and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,23 @@ class UsersController < ApplicationController
     end
   end
 
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'Car was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
   private
   def login_required
     unless current_user
