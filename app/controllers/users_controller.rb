490680579a91cd4ea6efe9a038ad6151b113c303
Ahmed A. Selim <ahmed.s3lim@gmail.com>
2012-12-31 13:31:19 -0800

    restrict to user module by login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-#  before_filter :login_required, :only => [:index ,:show]
+  before_filter :login_required, :only => [:index ,:show, :destroy]
   # GET /users
   # GET /users.json
   def index
