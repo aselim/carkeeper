bbb13e89e46a3f0c3ba4d41568a720565008e24b
Ahmed A. Selim <ahmed.s3lim@gmail.com>
2013-01-08 07:18:52 -0800

    Update Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,12 @@ users.each do |user|
   User.create!(user)
 end
 
-cars = [{datein: "2012-12-23 20:42:00", brand: "Toyota", carmodel: "Lexus", year: 2000, cc: 1500, used: false, km: 1000.0, user_id: 1, comment: "first car"},
-        {datein: "2012-12-23 20:42:00", brand: "Honda", carmodel: "Civic", year: 2005, cc: 1500, used: true, km: 2000.0, user_id: 2, comment: "nothing"},
-	{datein: "2012-12-24 20:42:00", brand: "Fiat", carmodel: "128", year: 1995, cc: 1200, used: true, km: 50000.0, user_id: 1, comment: "second car"}
+cars = [{datein: "2012-12-23 20:42:00", brand: "Toyota", carmodel: "Lexus", year: 2000, cc: 1500, used: false, km: 1000.0, user_id: 1, comment: "first car", price: "65000"},
+        {datein: "2012-12-23 20:42:00", brand: "Honda", carmodel: "Civic", year: 2005, cc: 1500, used: true, km: 2000.0, user_id: 2, comment: "nothing", price: "65000"},
+	{datein: "2012-12-24 20:42:00", brand: "Fiat", carmodel: "128", year: 1995, cc: 1200, used: true, km: 50000.0, user_id: 1, comment: "second car", price: "65000"},
+	{datein: "2012-12-23 20:42:00", brand: "Toyota", carmodel: "Lexus", year: 2000, cc: 1500, used: false, km: 1000.0, user_id: 1, comment: "first car", price: "65000"},
+        {datein: "2012-12-23 20:42:00", brand: "Honda", carmodel: "Civic", year: 2005, cc: 1500, used: true, km: 2000.0, user_id: 2, comment: "nothing", price: "65000"},
+        {datein: "2012-12-24 20:42:00", brand: "Fiat", carmodel: "128", year: 1995, cc: 1200, used: true, km: 50000.0, user_id: 1, comment: "second car", price: "65000"}
     	  ]
 
 cars.each do |car|
